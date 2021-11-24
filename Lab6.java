16223d3d7b7387de68fcfc303a0691816258b97c
Lev753 <79959074+Lev753@users.noreply.github.com>
2021-11-24 15:03:14 +0800

    Add files via upload

---
+++ Lab6.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class Lab6 {
+
+    public static void main(String[] args) {
+        int[] m = new int[12];
+        int posCnt = 0;
+        Random rnd = new Random();
+        for(int i=0;i<m.length;i++) {
+            m[i] = rnd.nextInt(10) + 1;
+            if(posCnt == m.length/2) {
+                m[i] = -m[i];
+            }
+            else if((m.length - i) > (m.length/2 - posCnt)) {
+              int sgn = rnd.nextInt(2);
+              m[i] = m[i] * (int)Math.pow(-1, sgn);
+              posCnt += -sgn + 1;
+            }
+            System.out.print(m[i] + "  ");
+        }
+    }
+}
